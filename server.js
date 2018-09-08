46d0cf23845d056455a4f78a35d48856298f1480
Roderick Trice <rocky_trice@yahoo.com>
2018-09-08 00:07:49 -0400

    "required request and cheerio"

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 // Initialize Express
 var app = express();
-
+var request = require("request");
+var cheerio = require("cheerio");
 // Set up a static folder (public) for our web app
 app.use(express.static("public"));

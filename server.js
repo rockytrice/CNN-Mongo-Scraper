68c382fcd4f3eb4892ff6364ed0711b4210a5a84
Roderick Trice <rocky_trice@yahoo.com>
2018-09-10 23:36:39 -0400

    adding new route for get all articles

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-
+// Use morgan logger for logging requests
+app.use(logger("dev"));
 // Set up a static folder (public) for our web app
 app.use(express.static("public"));
 
@@ -36,17 +37,12 @@ app.engine("handlebars", exphbs({
 app.set("view engine", "handlebars");
 
 // Database configuration====================================================================================
-// Save the URL of our database as well as the name of our collection
-var databaseUrl = "cnnscraper";
-var collections = ["scrapeData"];
-
-// Use mongojs to hook the database to the db variable
-var db = mongojs(databaseUrl, collections);
-
-// This makes sure that any errors are logged if mongodb runs into an issue
-db.on("error", function (error) {
-    console.log("Database Error:", error);
-});
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/cnnScraper", { useNewUrlParser: true });
+// // This makes sure that any errors are logged if mongodb runs into an issue
+// db.on("error", function (error) {
+//     console.log("Database Error:", error);
+// });
 // Routes=======================================================================================================
 app.get("/", function (req, res) {
     res.render("index", {
@@ -61,47 +57,58 @@ app.get("/scrape", function (req, res) {
         //     // load html body from request into cheerio
         var $ = cheerio.load(html);
         $("h3.cd__headline").each(function (i, element, ) {
-            var title = $(element).children().text();
-            var link = $(element).children("a").attr("href");
-            var summary = $(element).children("a").text();
-            if (title && link && summary) {
-                // Insert the data in the scrapedData db
-                db.scrapedData.insert({
-                        title: title,
-                        link: link,
-                        summary: summary,
-                    },
-                    function (err, inserted) {
-                        if (err) {
-                            // Log the error if one is encountered during the query
-                            console.log(err);
-                        } else {
-                            // Otherwise, log the inserted data
-                            console.log(inserted);
-                        }
-                    });
-            }
+            var result = {};
+            // Add the text,summary and href of every link, and save them as properties of the result object
+            result.title = $(this)
+             .children()
+             .text();
+            result.link = $(this)
+             .children("a")
+             .attr("href");
+            result.summary = $(this)
+             .children("a")
+             .text();
+
+
+            
+            // var title = $(element).children().text();
+            // var link = $(element).children("a").attr("href");
+            // var summary = $(element).children("a").text();
+            // if (title && link && summary) {
+            //     // Insert the data in the scrapedData db
+            //     db.scrapedData.insert({
+            //             title: title,
+            //             link: link,
+            //             summary: summary,
+            //         },
+            //         function (err, inserted) {
+            //             if (err) {
+            //                 // Log the error if one is encountered during the query
+            //                 console.log(err);
+            //             } else {
+            //                 // Otherwise, log the inserted data
+            //                 console.log(inserted);
+            //             }
+            //         });
+            // }
 
         })
     })
     // Send a "Scrape Complete" message to the browser
     res.send("Scrape Complete");
 });
-// find all article route================================================================================================
-// Retrieve results from mongo
+// Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
-    // Find all notes in the notes collection
-    db.scrapedData.find({}, function(error, found) {
-      // Log any errors
-      if (error) {
-        console.log(error);
-      }
-      else {
-        // Otherwise, send json of the notes back to user
-        // This will fire off the success function of the ajax request
-        res.json(found);
-      }
-    });
+    // Grab every document in the Articles collection
+    db.Article.find({})
+      .then(function(dbArticle) {
+        // If we were able to successfully find Articles, send them back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
   });
 
 

aa3a9fb53dcfabb5c4d24958cef8a4e2f7baf169
Roderick Trice <rocky_trice@yahoo.com>
2018-09-11 21:52:00 -0400

    changed the database for heroku

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.set("view engine", "handlebars");
 
 // Database configuration====================================================================================
 //  If deployed, use the deployed database. Otherwise use the local mongoHeadlines database 
- var MONGODB_URI = process.env.MONGODB_URI ||"mongodb://localhost/cnnScraper";
+ var MONGODB_URI = process.env.MONGODB_URI ||"mongodb://localhost/mongoHeadlines";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
